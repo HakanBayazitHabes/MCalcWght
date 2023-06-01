e7ba306ea9a16201affa999b0f445a7e0353e4e2
Hakan Bayazit Habes <hakanbayazithabes@gmail.com>
2023-06-02 00:35:04 +0300

    Added substraction - using substring methods

--- app/src/main/java/com/hakanbayazithabes/mcalcwght/MainActivity.kt
+++ app/src/main/java/com/hakanbayazithabes/mcalcwght/MainActivity.kt
@@ -46,6 +46,66 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var tvValue = binding.tvInput.text.toString()
+            var prefix = ""
+            try {
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if (tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    binding.tvInput.text =
+                        removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                } else if (tvValue.contains("+")) {
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    binding.tvInput.text =
+                        removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                } else if (tvValue.contains("*")) {
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    binding.tvInput.text =
+                        removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                } else if (tvValue.contains("/")) {
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if (!prefix.isEmpty()) {
+                        one = prefix + one
+                    }
+                    binding.tvInput.text =
+                        removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                }
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    private fun removeZeroAfterDot(result: String): String {
+        var value = result
+        if (result.contains(".0")) {
+            value = result.substring(0, result.length - 2)
+        }
+        return value
+    }
+
     private fun isOperatorAdded(value: String): Boolean {
         return if (value.startsWith("-")) {
             false
